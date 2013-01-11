ea7f8f0a3e3f188fbfd4d086a9f4494c75757944
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-01-11 17:59:40 +0100

    Upgraded pub/sub to allow for optional direction

--- README.md
+++ README.md
@@ -12,6 +12,14 @@ Methods
     Valid options:
     * **delimiter** - _string_ - The string to use as delimiter between keys.
     **Default:** '.'
+    * **subscribe** - _bool_ - Whether the syncer should subscribe to incomming
+    updates. If set to `false`, no incomming updates will be accepted.
+    **Default:** 'true'
+    * **publish** - _bool_ - Sets whether the syncer should publish updates. If
+    set to `false`, no updates will be sent.
+    **Default:** 'false'
+    **Note:** Two-way syncing can only be done if both sides have equal objects
+    at the time that the connection is made!
 
 Inherits all methods of [KVCObject](https://github.com/martinvl/KVCObject). All
 updates are automatically (and minimally) synced.
--- test/ObjSyncTest.js
+++ test/ObjSyncTest.js
@@ -10,87 +10,297 @@ suite('ObjSync', function() {
         this.server = io.listen(PORT).set('log level', 0);
     });
 
-    setup(function (done) {
-        var self = this;
-        this.server.once('connection', function (socket) {
-            self.serverSide = new ObjSync(socket);
-            done();
+
+    suite('internals', function() {
+        test('sets default options correctly', function () {
+            var sync = new ObjSync(this.server.sockets);
+
+            assert.equal(sync._options.subscribe, true);
+            assert.equal(sync._options.publish, false);
+
+            this.server.sockets.removeAllListeners();
+            sync.removeAllListeners();
         });
 
-        this.socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
-        this.clientSide = new ObjSync(this.socket);
-    });
+        test('sets options correctly', function () {
+            var sync = new ObjSync(this.server.sockets, {subscribe:false, publish:true});
 
-    teardown(function () {
-        this.socket.removeAllListeners();
-        this.socket.disconnect();
+            assert.equal(sync._options.subscribe, false);
+            assert.equal(sync._options.publish, true);
 
-        this.clientSide.removeAllListeners();
-        this.serverSide.removeAllListeners();
-    });
-
-    test('Emits updates from server side', function (done) {
-        var obj = {foo:'bar'};
-
-        this.clientSide.on('update', function (updated) {
-            assert.deepEqual(updated, obj);
-            done();
-        });
-
-        this.serverSide.setObject(obj);
-    });
-
-    test('Emits updates from client side', function (done) {
-        var obj = {foo:'bar'};
-
-        this.serverSide.on('update', function (updated) {
-            assert.deepEqual(updated, obj);
-            done();
-        });
-
-        this.clientSide.setObject(obj);
-    });
-
-    test('Emits mixed updates as expected', function (done) {
-        var firstObj = {foo:'bar'};
-        var secondObj = {foo:'bar', man:{name:'johnny'}};
-
-        var self = this;
-        this.clientSide.once('update', function (updated) {
-            assert.deepEqual(updated, firstObj);
-
-            self.clientSide.setObject(secondObj);
-        });
-
-        this.serverSide.setObject(firstObj);
-
-        this.serverSide.on('update', function (updated) {
-            assert.deepEqual(updated, {'man.name':'johnny'});
-
-            assert.deepEqual(self.serverSide.getObject(), secondObj);
-            assert.deepEqual(self.clientSide.getObject(), secondObj);
-
-            done();
+            this.server.sockets.removeAllListeners();
+            sync.removeAllListeners();
         });
     });
 
-    test('Emits deletes as expected', function (done) {
-        var obj = {foo:'bar', man:{name:'johnny'}};
-
-        var self = this;
-        this.serverSide.once('update', function (updated) {
-            assert.deepEqual(self.serverSide.getObject(), obj);
-
-            self.serverSide.setValueForKeypath(undefined, 'man.name');
+    suite('server side publish', function() {
+        setup(function () {
+            this.serverSide = new ObjSync(this.server.sockets, {publish:true, subscribe:false});
         });
 
-        this.clientSide.setObject(obj);
+        teardown(function () {
+            this.server.sockets.removeAllListeners();
+            this.serverSide.removeAllListeners();
+        });
 
-        this.clientSide.on('update', function (updated) {
-            assert.deepEqual(updated, {'man.name':undefined});
-            assert.deepEqual(self.clientSide.getObject(), {foo:'bar'});
+        test('emits base upon connection', function (done) {
+            var obj = {foo:'obj'};
+            this.serverSide.setObject(obj);
 
-            done();
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+
+            socket.on('create', function (base) {
+                assert.deepEqual(base, obj);
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('emits updates', function (done) {
+            var obj = {foo:'bar'};
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+
+            var self = this;
+            socket.on('connect', function () {
+                self.serverSide.setObject(obj);
+            });
+
+            socket.on('update', function (payload) {
+                assert.deepEqual(payload.updated, obj);
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('emits deletes', function (done) {
+            this.serverSide.setObject({foo:'bar', man:{name:'johnny'}});
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var obj = {foo:'bar'};
+
+            var self = this;
+            socket.on('connect', function () {
+                self.serverSide.setObject(obj);
+            });
+
+            socket.on('update', function () {
+                assert.deepEqual(self.serverSide.getObject(), obj);
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+    });
+
+    suite('client side publish', function() {
+        test('emits base upon connection', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket, {publish:true});
+
+            this.server.sockets.once('connection', function (socket) {
+                socket.on('create', function (base) {
+                    assert.deepEqual(base, {});
+
+                    socket.removeAllListeners();
+                    socket.disconnect();
+
+                    done()
+                });
+            });
+        });
+
+        test('emits updates', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket, {publish:true});
+
+            var obj = {foo:'bar'};
+
+            this.server.sockets.once('connection', function (socket) {
+                socket.on('create', function (base) {
+                    clientSide.setObject(obj);
+                });
+
+                socket.on('update', function (payload) {
+                    assert.deepEqual(payload.updated, obj);
+
+                    socket.removeAllListeners();
+                    socket.disconnect();
+
+                    done()
+                });
+            });
+        });
+
+        test('emits deletes', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket, {publish:true});
+
+            clientSide.setObject({foo:'bar', man:{name:'johnny'}});
+
+            var obj = {foo:'bar'};
+
+            this.server.sockets.once('connection', function (socket) {
+                socket.once('update', function (payload) {
+                    clientSide.setObject(obj);
+
+                    socket.on('update', function () {
+                        assert.deepEqual(clientSide.getObject(), obj);
+
+                        socket.removeAllListeners();
+                        socket.disconnect();
+
+                        done()
+                    });
+                });
+            });
+        });
+    });
+
+    suite('server side subscribe', function() {
+        setup(function () {
+            this.serverSide = new ObjSync(this.server.sockets);
+        });
+
+        teardown(function () {
+            this.server.sockets.removeAllListeners();
+            this.serverSide.removeAllListeners();
+        });
+
+        test('receives base upon connection', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket, {publish:true});
+            clientSide.setObject({foo:'bar'});
+
+            var self = this;
+            this.serverSide.on('update', function () {
+                assert.deepEqual(self.serverSide.getObject(), clientSide.getObject());
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('receives updates', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket, {publish:true});
+
+            socket.on('connect', function () {
+                clientSide.setObject({foo:'bar'});
+            });
+
+            var self = this;
+            this.serverSide.on('update', function () {
+                assert.deepEqual(self.serverSide.getObject(), clientSide.getObject());
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('receives deletes', function (done) {
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var obj = {foo:'bar'};
+
+            var clientSide = new ObjSync(socket, {publish:true});
+            clientSide.setObject({foo:'bar', man:{name:'johnny'}});
+
+            var self = this;
+            this.serverSide.once('update', function (payload) {
+                clientSide.setObject(obj);
+
+                self.serverSide.once('update', function () {
+                    assert.deepEqual(self.serverSide.getObject(), obj);
+
+                    socket.removeAllListeners();
+                    socket.disconnect();
+
+                    done();
+                });
+            });
+        });
+    });
+
+    suite('client side subscribe', function() {
+        setup(function () {
+            this.serverSide = new ObjSync(this.server.sockets, {publish:true});
+        });
+
+        teardown(function () {
+            this.server.sockets.removeAllListeners();
+            this.serverSide.removeAllListeners();
+        });
+
+        test('receives base upon connection', function (done) {
+            var obj = {foo:'bar'};
+            this.serverSide.setObject(obj);
+
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket);
+
+            clientSide.on('update', function (updated) {
+                assert.deepEqual(updated, obj);
+                assert.deepEqual(clientSide.getObject(), obj);
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('receives updates', function (done) {
+            var obj = {foo:'bar'};
+
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket);
+
+            var self = this;
+            socket.once('connect', function () {
+                self.serverSide.setObject(obj);
+            });
+
+            clientSide.on('update', function (updated) {
+                assert.deepEqual(updated, obj);
+                assert.deepEqual(clientSide.getObject(), obj);
+
+                socket.removeAllListeners();
+                socket.disconnect();
+
+                done();
+            });
+        });
+
+        test('receives deletes', function (done) {
+            this.serverSide.setObject({foo:'bar', man:{name:'johnny'}});
+            var obj = {foo:'bar'};
+
+            var socket = io_client.connect('http://localhost', {port:PORT, 'force new connection':true});
+            var clientSide = new ObjSync(socket);
+
+            var self = this;
+            clientSide.once('update', function (updated) {
+                self.serverSide.setObject(obj);
+
+                clientSide.on('update', function (updated) {
+                    assert.deepEqual(clientSide.getObject(), obj);
+
+                    socket.removeAllListeners();
+                    socket.disconnect();
+
+                    done();
+                });
+            });
         });
     });
 });
